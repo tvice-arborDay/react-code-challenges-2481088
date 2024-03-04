06ed667d949b33e5c69544ded8ade6062b044520
tvice-arborDay <tvice@arborday.org>
2024-03-04 15:27:50 -0600

    completed 09

--- src/09-simple-calculator/SimpleCalculator.js
+++ src/09-simple-calculator/SimpleCalculator.js
@@ -1,33 +1,96 @@
-const initialState = {}
+import { useReducer } from "react"
 
-function reducer (state, action) {}
+const initialState = {
+  num1: 0,
+  num2: 0,
+  result: null
+}
+
+function reducer (state, action) {
+  switch (action.type) {
+    case 'add_to_state': {
+      if (action.digit === 1) return {...state, num1: action.number, result: null}
+      if (action.digit === 2) return {...state, num2: action.number, result: null}
+      return state;
+    }
+    case 'sum': {
+        return {
+            ...state,
+            result: state.num1 + state.num2
+        }
+    }
+    case 'subtract': {
+      return {
+        ...state,
+        result: state.num1 - state.num2
+    }
+}
+    case 'clear': {
+        return initialState;
+    }
+    default: {
+        throw Error('Unknown action: ' + action.type);
+    }
+  }
+}
 
 export default function SimpleCalculator () {
   const numbers = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+  const [state, dispatch] = useReducer(reducer, initialState);
 
+  function handleNumberSelect(digit, selectedNumber) {
+    dispatch({
+      type: 'add_to_state',
+      digit: digit,
+      number: selectedNumber
+    })
+  }
+
+  function handleSum() {
+    dispatch({
+      type: 'sum'
+    })
+  }
+
+  function handleSubtract() {
+    dispatch({
+      type: 'subtract'
+    })
+  }
+
+  function handleClear() {
+    dispatch({
+      type: 'clear'
+    })
+  }
+  
   return (
     <div>
       <div>
         <h2>Number 1</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button key={number} onClick={() => handleNumberSelect(1, number)}>
             {number}
           </button>))}
       </div>
       <div>
         <h2>Number 2</h2>
         {numbers.map(number => (
-          <button key={number}>
+          <button key={number} onClick={() => handleNumberSelect(2, number)}>
             {number}
           </button>))}
       </div>
       <div>
-        <h2>Actions</h2>
-        <button>+</button>
-        <button>-</button>
-        <button>c</button>
+        <h2>Selected:</h2>
+        <h3>{state.num1}&nbsp;&nbsp;{state.num2}</h3>
       </div>
-      <div><h2>Result:</h2></div>
+      <div>
+        <h2>Actions</h2>
+        <button onClick={() => handleSum()}>+</button>
+        <button onClick={() => handleSubtract()}>-</button>
+        <button onClick={() => handleClear()}>c</button>
+      </div>
+      <div><h2>Result: {state.result ?? ''}</h2></div>
     </div>
   )
 }
